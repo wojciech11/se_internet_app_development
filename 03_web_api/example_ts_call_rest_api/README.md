ed6a7f98c3ea873132434e3f36c1464f361dbf10
wojciech11 <wojciech11@users.noreply.github.com>
2023-05-06 17:53:10 +0200

    update

---
+++ 03_web_api/example_ts_call_rest_api/README.md
@@ -0,0 +1,12 @@
+# Nodejs TS server calling external API
+
+```bash
+npm i -D typescript
+```
+
+```bash
+npx tsc server.ts
+```
+
+
+
---
+++ 03_web_api/example_ts_call_rest_api/server.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http = require("http");
+var axios_1 = require("axios");
+var hostname = "127.0.0.1";
+var port = (3000);
+var server = http.createServer(function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    axios_1.default
+        .get("https://mdn.github.io/learning-area/_javascript/oojs/json/superheroes.json")
+        .then(function (response) {
+        console.log(response.data);
+        res.statusCode = 200;
+        res.write(JSON.stringify(response.data));
+        res.end();
+    })
+        .catch(function (error) {
+        console.log(error);
+        res.statusCode = 500; // czy 424 byłby lepszy?
+        res.end(JSON.stringify({ error: error.message }));
+    });
+});
+server.listen(port, hostname, function () {
+    console.log("Server running at http://".concat(hostname, ":").concat(port, "/"));
+});
